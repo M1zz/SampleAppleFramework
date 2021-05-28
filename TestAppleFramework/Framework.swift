9d04d766cd77e22966e90298605ff7dfc08e9027
hyunho.lee <hyunho.lee@wadiz.kr>
2021-05-28 23:42:02 +0900

    first commit

---
+++ TestAppleFramework/Framework.swift
@@ -0,0 +1,15 @@
+//
+//  Framework.swift
+//  TestAppleFramework
+//
+//  Created by 이현호 on 2021/05/28.
+//
+
+import Foundation
+
+struct Framework {
+    let name: String
+    let imageName: String
+    let urlString: String
+    let description: String
+}
---
+++ TestAppleFramework/TestAppleFrameworkApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestAppleFrameworkApp.swift
+//  TestAppleFramework
+//
+//  Created by 이현호 on 2021/05/28.
+//
+
+import SwiftUI
+
+@main
+struct TestAppleFrameworkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
