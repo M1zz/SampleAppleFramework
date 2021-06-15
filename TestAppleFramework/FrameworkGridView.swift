3643d0333f9ba9666febc67ae184359fd34ad877
hyunho.lee <mizzking75@gmail.com>
2021-06-15 23:52:46 +0900

    Add viewModel

--- TestAppleFramework/FrameworkGridView.swift
+++ TestAppleFramework/FrameworkGridView.swift
@@ -8,6 +8,9 @@
 import SwiftUI
 
 struct FrameworkGridView: View {
+    
+    @StateObject var viewModel = FrameworkGridViewModel()
+    
     let columns: [GridItem] = [GridItem(.flexible()),
                                GridItem(.flexible()),
                                GridItem(.flexible())]
